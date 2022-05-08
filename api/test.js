84930e2f5179e1539a799e716ef6e0807417205e
Dhyey Thumar <dhyeythumar@gmail.com>
2022-05-08 14:07:28 +0530

    feat: added test endpoint

---
+++ api/test.js
@@ -0,0 +1,29 @@
+import { getMiddleware } from "../src/middlewares.js";
+import { NetworkInfo } from "../src/serverInfo.js";
+import axios from "axios";
+
+const handler = async (_, res) => {
+    const myIPService = await axios({
+        url: `${process.env.HOST}/whatismyipaddress`,
+        method: "GET",
+        headers: {
+            "x-api-key": `${process.env.X_API_KEY}`,
+        },
+    });
+    const otherIPService = await axios({
+        url: "https://icanhazip.com/",
+        method: "GET",
+    });
+
+    const download = await NetworkInfo.calDownloadSpeed();
+    const upload = await NetworkInfo.calUploadSpeed();
+
+    res.status(200).json({
+        myIPService: myIPService.data.ip,
+        otherIPService: otherIPService.data.trim(),
+        ...download,
+        ...upload,
+    });
+};
+
+export default getMiddleware(handler);
