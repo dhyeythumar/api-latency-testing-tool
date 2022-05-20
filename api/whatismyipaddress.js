f9c0d8b233abfd57be56237b32f114a583f188cc
dhyeythumar <dhyeythumar@gmail.com>
2022-05-20 19:59:51 +0530

    removed aws files

--- api/whatismyipaddress.js
+++ api/whatismyipaddress.js
@@ -3,6 +3,7 @@ import { httpMiddleware } from "../src/middlewares.js";
 //* GET endpoint for getting server's IP
 //* usage: /whatismyipaddress
 const handler = async (req, res) => {
+    console.log(req.headers);
     // req.socket.remoteAddress will mostly give local/private IP address
     res.status(200).json({
         ip: req.headers["x-real-ip"] || req.socket.remoteAddress,
