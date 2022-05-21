b0b80162151f698e6c5174c66974ead8594e5e13
dhyeythumar <dhyeythumar@gmail.com>
2022-05-21 13:46:40 +0530

    refactored: add options & functions to lru cache class

--- src/lruCache.js
+++ src/lruCache.js
@@ -9,6 +9,8 @@ import LRU from "lru-cache";
 const cacheOptions = {
     max: 5000, // maximum number of items allowed in the cache
     ttl: 15 * 60 * 1000, // maximum life of a cached item in ms (15 min)
+    ttlAutopurge: false, // will degrade performance if its true
+    noUpdateTTL: true, // will not update ttl when new value is set to existing key
 };
 
 export default class LruCache {
@@ -27,5 +29,25 @@ export default class LruCache {
 
     set(key, data) {
         this.cache.set(key, data);
+    }
+
+    // remove specific entry from cache
+    delete(key) {
+        this.cache.delete(key);
+    }
+
+    // empty the cache
+    clear() {
+        this.cache.clear();
+    }
+
+    // remove all stale entries
+    purgeStale() {
+        const didCachePurge = this.cache.purgeStale();
+        if (didCachePurge) console.log("Purged all stale entires");
+    }
+
+    getRemainingTTL(key) {
+        this.cache.getRemainingTTL(key);
     }
 }
